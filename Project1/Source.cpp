9cdf9dbb1df51d70bf490892c27d2f405423b303
IsaacMo12 <48593866+IsaacMo12@users.noreply.github.com>
2019-03-24 20:33:30 +0200

    commit2

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -2,21 +2,23 @@
 #include <iostream>
 #include <fstream>
 #include "Menu.h"
+#include "setting.h"
 #include <SFML/Window.hpp>
 using namespace sf;
 using namespace std;
 int const rows = 31;
 int const cols = 28;
+int choose;
 int main()
 {
-	int map4[rows][cols];
+	int map4[50][50];
 	fstream inputStream;
 	inputStream.open("maps/map4.txt");
 	for (int i = 0; i < rows; i++)
 		for (int j = 0; j < cols; j++)
 			inputStream >> map4[i][j];
 
-
+ss:
 	RenderWindow startScreen(sf::VideoMode(1600, 900), "Pacman", Style::Close | Style::Resize);
 	Menu menu(startScreen.getSize().x, startScreen.getSize().y);
 
@@ -29,7 +31,7 @@ int main()
 	sprite.setColor(Color(255, 255, 255, 64));
 
 
-	while (startScreen.isOpen())
+    while (startScreen.isOpen())
 	{
 		Event event;
 
@@ -56,11 +58,13 @@ int main()
 					switch (menu.GetPressedItem())
 					{
 					case 0:
+						choose = 1;
 						startScreen.close();
 						break;
 
 					case 1:
-						cout << "Settings button has been pressed " << endl;
+						choose = 2;
+						startScreen.close();
 						break;
 
 					case 2:
@@ -87,57 +91,103 @@ int main()
 		startScreen.display();
 	}
 
-
-
-
-
-	RenderWindow pacman(VideoMode(1600, 900), "Pacman");
-	Texture player;
-	player.loadFromFile("img/pac.png");
-	Sprite player_s(player);
-	player_s.setPosition(Vector2f(32, 32));
-	player_s.setTextureRect(sf::IntRect(0, 4, 28, 28));
-
-	Texture wall;
-	wall.loadFromFile("img/wall.png");
-	Sprite wall_s(wall);
-
-
-	while (pacman.isOpen())
+	
+	//-----------------------Setting----------------------
+	if (choose == 2)
 	{
-		Event event;
-		while (pacman.pollEvent(event))
-		{
-			if (event.type == Event::Closed)
-				pacman.close();
-		}
-		pacman.clear();
-		for (int i = 0; i < rows; i++)
-			for (int j = 0; j < cols; j++)
-			{
-				if (map4[i][j] == 1) {
-					wall_s.setTextureRect(IntRect(0, 0, 40, 40));
-					wall_s.setPosition(j * 40, i * 40);
-					pacman.draw(wall_s);
-				}
-				if (player_s.getGlobalBounds().intersects(wall_s.getGlobalBounds()))
-				{
+		sf::RenderWindow settingScreen(sf::VideoMode(1600, 900), "Hello Sfml!");
+		Setting setting(settingScreen.getSize().x, settingScreen.getSize().y);
 
+		while (choose == 2 && settingScreen.isOpen())
+		{
+			sf::Event event;
+			while (settingScreen.pollEvent(event))
+			{
+				switch (event.type)
+				{
+				case Event::KeyReleased:
+					switch (event.key.code)
+					{
+					case::Keyboard::Up:
+						setting.MoveUp();
+						break;
+					case::Keyboard::Down:
+						setting.MoveDown();
+						break;
+					case::Keyboard::Return:
+						switch (setting.GetPressedItem())
+						{
+						case 0:
+							cout << "You pressed controls" << endl;
+							break;
+						case 1:
+							cout << "you prssed leader board" << endl;
+							break;
+						case 2:
+							goto ss;
+							break;
+						}
+						break;
+					}
+					break;
+				case sf::Event::Closed:
+					choose = 0;
+					settingScreen.close();
+					break;
 				}
 			}
+			settingScreen.clear();
+			settingScreen.draw(sprite);
+			setting.draw(settingScreen);
+			settingScreen.display();
+		}
+	}
+	//------------------------------------------------------------------
 
-		pacman.draw(player_s);
-		pacman.display();
+
+	if (choose == 1) 
+	{
+		RenderWindow pacman(VideoMode(1600, 900), "Pacman");
+		Texture player;
+		player.loadFromFile("img/pac.png");
+		Sprite player_s(player);
+		player_s.setPosition(Vector2f(32, 32));
+		player_s.setTextureRect(sf::IntRect(0, 4, 28, 28));
+
+		Texture wall;
+		wall.loadFromFile("img/wall.png");
+		Sprite wall_s(wall);
+
+
+		while (pacman.isOpen())
+		{
+			Event event;
+			while (pacman.pollEvent(event))
+			{
+				if (event.type == Event::Closed)
+					pacman.close();
+			}
+
+			pacman.clear();
+			for (int i = 0; i < rows; i++)
+				for (int j = 0; j < cols; j++)
+				{
+					if (map4[i][j] == 1)
+					{
+						wall_s.setTextureRect(IntRect(0, 0, 32, 32));
+						wall_s.setPosition(j * 32, i * 32);
+						pacman.draw(wall_s);
+
+						if (player_s.getGlobalBounds().intersects(wall_s.getGlobalBounds()))
+						{
+
+						}
+					}
+				}
+
+			pacman.draw(player_s);
+			pacman.display();
+		}
 	}
 
-
 }
-
-
-
-
-
-
-
-
-
---
+++ Project1/setting.cpp
@@ -0,0 +1,65 @@
+#include "Setting.h"
+#include <SFML/Graphics.hpp>
+#include <iostream>
+using namespace std;
+
+Setting::Setting(float width, float height)
+{
+	if (!font.loadFromFile("ARIAL.ttf"))
+	{
+		cout << "Error" << endl;
+
+	}
+	setting[0].setFont(font);
+	setting[0].setFillColor(sf::Color::Yellow);
+	setting[0].setString("Controls");
+	setting[0].setPosition(sf::Vector2f(200, 200));
+
+	setting[1].setFont(font);
+	setting[1].setFillColor(sf::Color::White);
+	setting[1].setString("Leader Board");
+	setting[1].setPosition(sf::Vector2f(200, 300));
+
+	setting[2].setFont(font);
+	setting[2].setFillColor(sf::Color::White);
+	setting[2].setString("Back");
+	setting[2].setPosition(sf::Vector2f(200, 400));
+
+	selectedItemIndex = 0;
+}
+
+
+
+
+void Setting::draw(sf::RenderWindow &window)
+{
+	for (int i = 0; i < MAX_NUMBER_OF_ITEMS; i++)
+	{
+		window.draw(setting[i]);
+	}
+
+}
+
+
+
+void Setting::MoveUp()
+{
+	if (selectedItemIndex - 1 >= 0)
+	{
+		setting[selectedItemIndex].setFillColor(sf::Color::White)
+			;	selectedItemIndex--;
+		setting[selectedItemIndex].setFillColor(sf::Color::Yellow)
+			;
+	}
+}
+
+void Setting::MoveDown()
+{
+	if (selectedItemIndex + 1 < MAX_NUMBER_OF_ITEMS)
+	{
+		setting[selectedItemIndex].setFillColor(sf::Color::White)
+			;	selectedItemIndex++;
+		setting[selectedItemIndex].setFillColor(sf::Color::Yellow)
+			;
+	}
+}
